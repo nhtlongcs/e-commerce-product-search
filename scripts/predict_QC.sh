5cfccd98236bb933bfa9c0cee10c5e1e130dbec6
nhtlongcs <thanglong.nguyenho27@mail.dcu.ie>
2025-09-18 14:53:46 +0100

    final dev

--- scripts/predict_QC.sh
+++ scripts/predict_QC.sh
@@ -1,7 +1,7 @@
 # QC - stage 1
 accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QC-stage-01/gemma-3-QC_2lang-2048-fold0-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-10000 config/QC/large_model_full.json config/QC/data_fold0.json
-accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QC-stage-01/gemma-3-QC_2lang-2048-fold1-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-11500 config/QC/large_model_full.json config/QC/data_fold1.json
-accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QC-stage-01/gemma-3-QC_2lang-2048-fold2-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-15000 config/QC/large_model_full.json config/QC/data_fold2.json
+# accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QC-stage-01/gemma-3-QC_2lang-2048-fold1-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-11500 config/QC/large_model_full.json config/QC/data_fold1.json
+# accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QC-stage-01/gemma-3-QC_2lang-2048-fold2-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-15000 config/QC/large_model_full.json config/QC/data_fold2.json
 
 # QC - stage 2
 # accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QC-stage-02/gemma-3-QC_2lang-2048-fold0-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-10000 config/QC/large_model_full.json config/QC/data_fold0.json
--- scripts/predict_QI.sh
+++ scripts/predict_QI.sh
@@ -1,7 +1,7 @@
 # QI - stage 1
 accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QI-stage-01/gemma_3_12b_pt-QI_2lang-2048-fold0-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-8000 config/QI/large_model_full.json config/QI/data_fold0.json
-accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QI-stage-01/gemma_3_12b_pt-QI_2lang-2048-fold1-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-8500 config/QI/large_model_full.json config/QI/data_fold1.json
-accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QI-stage-01/gemma_3_12b_pt-QI_2lang-2048-fold2-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-7500 config/QI/large_model_full.json config/QI/data_fold2.json
+# accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QI-stage-01/gemma_3_12b_pt-QI_2lang-2048-fold1-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-8500 config/QI/large_model_full.json config/QI/data_fold1.json
+# accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QI-stage-01/gemma_3_12b_pt-QI_2lang-2048-fold2-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-7500 config/QI/large_model_full.json config/QI/data_fold2.json
 
 # QI - stage 2
 # accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QI-stage-02/gemma-3-QI_2lang-2048-fold0-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-8000 config/QI/large_model_full.json config/QI/data_fold0.json
