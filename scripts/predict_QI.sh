266332b02a983e13001f6004c07551d7629dbe5a
nhtlongcs <thanglong.nguyenho27@mail.dcu.ie>
2025-09-26 13:21:55 +0100

    script pred

--- scripts/predict_QI.sh
+++ scripts/predict_QI.sh
@@ -9,5 +9,5 @@
 # accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/QI-stage-02/gemma-3-QI_2lang-2048-fold2-lr-1e-05-bf16-SchedulerType.CONSTANT/checkpoint-7500 config/QI/large_model_full.json config/QI/data_fold2.json
 
 # Choose between stage 1 and stage 2 models for inference (only need to run one of these)
-accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/best-gemma-3-QI-stage-01 config/QI/large_model_full.json config/QI/data_fold0.json
+# accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/best-gemma-3-QI-stage-01 config/QI/large_model_full.json config/QI/data_fold0.json
 accelerate launch --mixed_precision bf16 --multi_gpu  predict.py models/best-gemma-3-QI-stage-02 config/QI/large_model_full.json config/QI/data_fold0.json
